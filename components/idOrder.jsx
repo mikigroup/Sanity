46d7aa010ff7298417f88131bbde9238f5b6e7ef
mikigroup <39709130+mikigroup@users.noreply.github.com>
2023-03-10 11:13:42 +0100

    propis _id do GUI

--- components/idOrder.jsx
+++ components/idOrder.jsx
@@ -1,3 +1,23 @@
-export const idOrder = (props) => {
-  return <div>Value: {props.value}</div>
+// dokumentace https://www.sanity.io/docs/studio-react-hooks
+
+import {useMemoObservable} from 'react-rx'
+import { useFormValue } from 'sanity'
+import { useDocumentStore } from 'sanity'
+
+ export function idOrder() {
+  const docId = useFormValue(['_id'])
+  const documentStore = useDocumentStore();
+  const results = useMemoObservable(() => {
+    return documentStore.listenQuery(
+      `*[_type == 'order']`,
+      {currentDoc: docId},
+      {}
+    )
+  }, [documentStore, docId])
+  
+	return (
+	  <div>Value: {docId}</div>
+	)
 }
+
+
