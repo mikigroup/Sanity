260aa137937d9545fbabaf3c7a76f00b404a8e11
mikigroup <mikigroup@gmail.com>
2024-10-25 19:16:03 +0200

    doplnen listopad

--- deskStructure.js
+++ deskStructure.js
@@ -294,7 +294,7 @@ export default (S) =>
                               '_type == "menu" && releaseDate > "2024-09-30" && releaseDate < "2024-11-01"'
                             )
                             .params({ datefilter })
-                        ),/*
+                        ),
                       S.listItem()
                         .title("Listopad")
                         .schemaType("menu")
@@ -305,7 +305,7 @@ export default (S) =>
                               '_type == "menu" && releaseDate > "2024-10-31" && releaseDate < "2024-12-01"'
                             )
                             .params({ datefilter })
-                        ),
+                        ),/*
                       S.listItem()
                         .title("Prosinec")
                         .schemaType("menu")
