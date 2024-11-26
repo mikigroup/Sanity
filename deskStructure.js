7d424157c75af31297d6084cf1e356c7c71fcc35
mikigroup <39709130+mikigroup@users.noreply.github.com>
2024-11-26 17:55:02 +0100

    1

--- deskStructure.js
+++ deskStructure.js
@@ -319,6 +319,146 @@ export default (S) =>
                         ),
                     ])
                 ),
+                S.listItem()
+                    .title("2025")
+                    .child(
+                        S.list()
+                            .title("Měsíce")
+                            .items([
+                                S.listItem()
+                                    .title("Leden")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Leden")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2024-12-31" && releaseDate < "2025-02-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Únor")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Únor")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-01-31" && releaseDate < "2025-03-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Březen")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Březen")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-02-28" && releaseDate < "2025-04-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Duben")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Duben")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-03-31" && releaseDate < "2025-05-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Květen")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Květen")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-04-30" && releaseDate < "2025-06-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Červen")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Červen")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-05-31" && releaseDate < "2025-07-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Červenec")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Červenec")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-06-30" && releaseDate < "2025-08-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Srpen")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Srpen")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-07-31" && releaseDate < "2025-09-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Září")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Září")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-08-31" && releaseDate < "2025-10-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Říjen")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Říjen")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-09-30" && releaseDate < "2025-11-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Listopad")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Listopad")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-10-31" && releaseDate < "2025-12-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Prosinec")
+                                    .schemaType("menu")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("menu")
+                                            .title("Meníčka Prosinec")
+                                            .filter(
+                                                '_type == "menu" && releaseDate > "2025-11-30" && releaseDate < "2026-01-01"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                            ])
+                    ),
             ])
         ),
       /*  S.listItem()
@@ -566,6 +706,146 @@ export default (S) =>
                         ),
                     ])
                 ),
+                S.listItem()
+                    .title("2025")
+                    .child(
+                        S.list()
+                            .title("Měsíce")
+                            .items([
+                                S.listItem()
+                                    .title("Leden")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Leden")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-01-01T00:00:00Z" && _createdAt < "2025-02-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Únor")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Únor")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-02-01T00:00:00Z" && _createdAt < "2025-03-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Březen")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Březen")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-03-01T00:00:00Z" && _createdAt < "2025-04-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Duben")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Duben")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-04-01T00:00:00Z" && _createdAt < "2025-05-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Květen")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Květen")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-05-01T00:00:00Z" && _createdAt < "2025-06-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Červen")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Červen")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-06-01T00:00:00Z" && _createdAt < "2025-07-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Červenec")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Červenec")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-07-01T00:00:00Z" && _createdAt < "2025-08-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Srpen")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Srpen")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-08-01T00:00:00Z" && _createdAt < "2025-09-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Září")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Září")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-09-01T00:00:00Z" && _createdAt < "2025-10-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Říjen")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Říjen")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-10-01T00:00:00Z" && _createdAt < "2025-11-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Listopad")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Listopad")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-11-01T00:00:00Z" && _createdAt < "2025-12-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                                S.listItem()
+                                    .title("Prosinec")
+                                    .schemaType("order")
+                                    .child((datefilter) =>
+                                        S.documentTypeList("order")
+                                            .title("Objednávky Prosinec")
+                                            .filter(
+                                                '_type == "order" && _createdAt >= "2025-12-01T00:00:00Z" && _createdAt < "2026-01-01T00:00:00Z"'
+                                            )
+                                            .params({ datefilter })
+                                    ),
+                            ])
+                    ),
             ])
         ),
     ]);
